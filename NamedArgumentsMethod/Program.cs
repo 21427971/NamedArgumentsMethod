28a48d4d5e11622f5ef6e0097fd3d10dfbdfc85d
msimangoksi@gmail.com <msimangoksi@gmail.com>
2023-04-26 15:31:48 +0200

    Add project files.

---
+++ NamedArgumentsMethod/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Msimango KSI
+namespace NamedArgumentsMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            VisitPlanets();
+            VisitPlanets(numberOfPlanets: 2);
+            VisitPlanets(numberOfPlanets: 2, name: "Khayelihle Msimango");
+
+        }
+
+        static void VisitPlanets(
+          string adjective = "brave",
+          string name = "Cosmonaut",
+          int numberOfPlanets = 0,
+          double gForce = 4.2)
+        {
+            Console.WriteLine($"Welcome back, {adjective} {name}.");
+            Console.WriteLine($"You visited {numberOfPlanets} new planets...");
+            Console.WriteLine($"...while experiencing a g-force of {gForce} g!");
+        }
+    }
+}
